fadf905039c86571b9cbd72263314c5b146455d7
John Wittenauer <jdwittenauer@gmail.com>
2016-04-11 20:49:37 -0400

    Getting some of the flask basics set up

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ app.config['SECRET_KEY'] = 'top secret'
 
 
 @app.route('/', methods=['GET'])
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello/', methods=['GET'])
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
